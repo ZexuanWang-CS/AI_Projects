e504fec9725e85885af0815e05d7fe487baa3717
Zexuan Wang <zwang131@UR.Rochester.edu>
2018-12-12 00:58:38 -0500

    Upload Decision Tree

---
+++ MachineLearning/DecisionTreeLearning_ID3/DecisionTreeNode.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+public class DecisionTreeNode {
+
+	public String Par;
+	public String ParVal;
+	public String Self;
+	public ArrayList<DecisionTreeNode> Children;
+
+	public DecisionTreeNode(String par, String parVal, String self, ArrayList<DecisionTreeNode> children) {
+		this.Par = par;
+		this.ParVal = parVal;
+		this.Self = self;
+		this.Children = new ArrayList<DecisionTreeNode>();
+		if (children != null) {
+			for (DecisionTreeNode child : children) {
+				Children.add(child);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
